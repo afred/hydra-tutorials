e69ac82358f40154b8e733f59270d9b21db5dd03
Andrew Myers <andrew_myers@wgbh.org>
2013-06-03 00:20:54 -0400

    Created new rails 3.2.13 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiveIntoHydra::Application.config.secret_token = '01acfc231b295efdec171212d4820b9e47c615ff8eff241b7a00c512672409bf80bd59ec3efacd763d9b5ea213de059778c146b5d43d46fd5b57ef09036b515f'
